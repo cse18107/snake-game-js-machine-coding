70bc6b0d3d947d8cd581c9dc009fdd0351eff198
cse18107 <cse18107@cemk.ac.in>
2024-09-01 10:46:08 +0530

    added snake moving logic

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ document.addEventListener('DOMContentLoaded', function () {
     let food = { x: 300, y: 200 }; // { x: 15*20, y: 10*20 } // -> cell coordinate -> pixels
     let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}];
 
+    let dx = cellSize;
+    let dy = 0;
+
+    function updateSnake() {
+        const newHead = { x: snake[0].x + dx, y: snake[0].y + dy  };
+        snake.unshift(newHead);
+        // check collision with food
+        if(newHead.x === food.x && newHead.y === food.y) {
+            score += 10;
+            document.getElementById('score-board').textContent = `Score: ${score}`;
+            // generateFood();
+        }else {
+            snake.pop();
+        }
+    }
+
+    function changeDirection(e) {
+        const isGoingDown = dy === cellSize;
+        const isGoingUp = dy === -cellSize;
+        const isGoingRight = dx === cellSize;
+        const isGoingLeft = dx === -cellSize;
+        if(e.key === 'ArrowUp' && !isGoingDown){
+            dx=0;
+            dy = -cellSize;
+        }else if(e.key === 'ArrowDown' && !isGoingUp) {
+            dx = 0;
+            dy = cellSize;
+        }else if(e.key === 'ArrowLeft' && !isGoingRight) {
+            dx = -cellSize;
+            dy = 0;
+        }else if(e.key === 'ArrowRight' && !isGoingLeft) {
+            dx = cellSize;
+            dy = 0;
+        }
+    }
+
     function drawDiv(x, y, className) {
         const divElement = document.createElement('div');
         divElement.classList.add(className);
@@ -29,11 +65,19 @@ document.addEventListener('DOMContentLoaded', function () {
         gameArena.appendChild(foodElement)
     }
 
+    function gameLoop() {
+        setInterval(() => {
+            updateSnake();
+            drawFoodAndSnake();
+        },200);
+    }
+
     function runGame() {
         if(!gameStarted){
             gameStarted = true;
-            // gameLoop();
-            drawFoodAndSnake()
+            document.addEventListener('keydown', changeDirection);
+            // drawFoodAndSnake();
+            gameLoop();
         }
     }
 
@@ -51,10 +95,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
         startButton.addEventListener('click', function startGame() {
             startButton.style.display = 'none';
+            runGame();
         })
 
         document.body.appendChild(startButton);// Append start button to the body
-        runGame() 
+        
     }
 
     initiateGame();
